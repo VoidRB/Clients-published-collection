392112bb901745f329d1478a701eeb3227642bc0
NinesVoid <ahmedalkhair47@gmail.com>
2026-03-09 17:26:47 +0300

    fix: another deployment fix

--- Client/src/components/PublicPictureGallery.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-import type Picture from "@/interfaces/pictureInterface";
-</script>
-<template>
-  <div class="columns-1 gap-4 space-y-4 p-4 px-10 sm:columns-2 lg:columns-3"></div>
-</template>
