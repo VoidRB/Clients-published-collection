3470c0880204fa557efcf915c3d231d28ebc56ef
NinesVoid <ahmedalkhair47@gmail.com>
2026-04-14 15:11:07 +0300

    fix: navbar list padding was off, now fixed

--- Client/src/components/navbar/NavBar.vue
+++ Client/src/components/navbar/NavBar.vue
@@ -4,18 +4,17 @@ import NavBarDropdown from "./NavBarDropdown.vue";
 import NavBarList from "./NavBarList.vue";
 </script>
 <template>
-  <div
-    class="navbar text-neutral fixed top-0 isolate z-50 shadow-sm backdrop-blur-sm text-shadow-md"
-  >
+  <div class="navbar text-neutral fixed top-0 isolate z-50 shadow-sm backdrop-blur-sm">
     <div class="navbar-start">
       <RouterLink
         to="/"
-        class="mr-4 flex gap-2 text-2xl text-nowrap duration-75 hover:scale-110 hover:cursor-pointer hover:drop-shadow-2xl"
+        class="mr-4 flex text-2xl text-nowrap duration-75 hover:scale-110 hover:cursor-pointer hover:drop-shadow-2xl"
         ><img draggable="false" src="/logo.svg" class="size-6" />أرشيف الحصين</RouterLink
       >
     </div>
     <div class="navbar-end">
-      <NavBarDropdown class="block lg:hidden" /><NavBarList class="hidden lg:block" />
+      <NavBarDropdown class="block lg:hidden" />
+      <NavBarList class="hidden lg:block" />
     </div>
   </div>
 </template>
--- Client/src/components/navbar/NavBarList.vue
+++ Client/src/components/navbar/NavBarList.vue
@@ -3,15 +3,18 @@ import { RouterLink } from "vue-router";
 </script>
 <template>
   <div>
-    <ul class="menu menu-horizontal flex items-center justify-center text-xl">
+    <ul class="menu menu-horizontal flex gap-2 text-xl">
       <li>
-        <RouterLink to="/books" class="duration-75 hover:scale-110">الكتب</RouterLink>
+        <RouterLink to="/books" class="p-0 px-2 duration-75 hover:scale-110">الكتب</RouterLink>
+      </li>
+
+      <li>
+        <RouterLink to="/articles" class="p-0 px-2 duration-75 hover:scale-110"
+          >المقالات</RouterLink
+        >
       </li>
       <li>
-        <RouterLink to="/articles" class="duration-75 hover:scale-110">المقالات</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/pictures" class="duration-75 hover:scale-110">الصور</RouterLink>
+        <RouterLink to="/pictures" class="p-0 px-2 duration-75 hover:scale-110">الصور</RouterLink>
       </li>
     </ul>
   </div>
